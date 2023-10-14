b0629b297bdd589513f438c3c26da0e931519f42
TAN TING YEE <ttyseed@hotmail.sg>
2023-10-14 11:22:07 +0800

    add in localDB

---
+++ BackendService/TodolistAPI/TodolistAPI/Database/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodolistAPI.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
--- BackendService/TodolistAPI/TodolistAPI/Program.cs
+++ BackendService/TodolistAPI/TodolistAPI/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
+using TodolistAPI.Database;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseInMemoryDatabase("ApplicationDb"));
 
 var app = builder.Build();
 
